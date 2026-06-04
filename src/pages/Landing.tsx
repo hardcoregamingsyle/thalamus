0a27965d868a3cba71eab2d6abeabd8d4f380ad6
hardcoregamingsyle <hardcorgamingstyle@gmail.com>
2026-06-04 13:15:22 +0000

    Fix landing download link to v7.0.2 installer

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -332,7 +332,7 @@ function IntelligenceConsole({ onLaunch }: { onLaunch: () => void }) {
   );
 }
 
-const INSTALLER_URL = "https://github.com/hardcoregamingsyle/thalamus/releases/download/thalamus-installer-v7.0.1/thalamus-installer-v7.0.1.exe";
+const INSTALLER_URL = "https://github.com/hardcoregamingsyle/thalamus/releases/download/thalamus-installer-v7.0.2/thalamus-installer-v7.0.2.exe";
 
 function isWindows(): boolean {
   if (typeof navigator === "undefined") return false;
