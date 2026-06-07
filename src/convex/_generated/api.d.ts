76f60ec2cd009b42f5338d20d6b4f40e563e9998
hardcoregamingsyle <hardcorgamingstyle@gmail.com>
2026-06-07 19:31:25 +0530

    tune convex backend + admin backend

--- src/convex/_generated/api.d.ts
+++ src/convex/_generated/api.d.ts
@@ -8,10 +8,48 @@
  * @module
  */
 
+import type * as admin from "../admin.js";
+import type * as agentCore from "../agentCore.js";
+import type * as agentPipeline from "../agentPipeline.js";
+import type * as agentTeamHelpers from "../agentTeamHelpers.js";
+import type * as ai from "../ai.js";
+import type * as aiHelpers from "../aiHelpers.js";
+import type * as antiEvasionDb from "../antiEvasionDb.js";
 import type * as auth from "../auth.js";
 import type * as auth_emailOtp from "../auth/emailOtp.js";
+import type * as codeApiKeys from "../codeApiKeys.js";
+import type * as codeBranches from "../codeBranches.js";
+import type * as codeCommands from "../codeCommands.js";
+import type * as codeMigration from "../codeMigration.js";
+import type * as codePipeline from "../codePipeline.js";
+import type * as codeProjects from "../codeProjects.js";
+import type * as conversations from "../conversations.js";
+import type * as crons from "../crons.js";
+import type * as customAuth from "../customAuth.js";
+import type * as customAuthHelpers from "../customAuthHelpers.js";
+import type * as dailyReset from "../dailyReset.js";
+import type * as deployments from "../deployments.js";
+import type * as fileSync from "../fileSync.js";
+import type * as github from "../github.js";
+import type * as githubAutoCreate from "../githubAutoCreate.js";
+import type * as githubHelpers from "../githubHelpers.js";
+import type * as githubQueries from "../githubQueries.js";
+import type * as githubStorage from "../githubStorage.js";
+import type * as githubSync from "../githubSync.js";
+import type * as githubSyncHelpers from "../githubSyncHelpers.js";
+import type * as githubWebhooks from "../githubWebhooks.js";
+import type * as hfRagSpace from "../hfRagSpace.js";
 import type * as http from "../http.js";
+import type * as obscureRepoGenerator from "../obscureRepoGenerator.js";
+import type * as qemuSandbox from "../qemuSandbox.js";
+import type * as rag from "../rag.js";
+import type * as ragHelpers from "../ragHelpers.js";
+import type * as sandbox from "../sandbox.js";
+import type * as sandboxHelpers from "../sandboxHelpers.js";
+import type * as study from "../study.js";
+import type * as studyHelpers from "../studyHelpers.js";
 import type * as users from "../users.js";
+import type * as v86Sandbox from "../v86Sandbox.js";
 
 import type {
   ApiFromModules,
@@ -20,10 +58,48 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  admin: typeof admin;
+  agentCore: typeof agentCore;
+  agentPipeline: typeof agentPipeline;
+  agentTeamHelpers: typeof agentTeamHelpers;
+  ai: typeof ai;
+  aiHelpers: typeof aiHelpers;
+  antiEvasionDb: typeof antiEvasionDb;
   auth: typeof auth;
   "auth/emailOtp": typeof auth_emailOtp;
+  codeApiKeys: typeof codeApiKeys;
+  codeBranches: typeof codeBranches;
+  codeCommands: typeof codeCommands;
+  codeMigration: typeof codeMigration;
+  codePipeline: typeof codePipeline;
+  codeProjects: typeof codeProjects;
+  conversations: typeof conversations;
+  crons: typeof crons;
+  customAuth: typeof customAuth;
+  customAuthHelpers: typeof customAuthHelpers;
+  dailyReset: typeof dailyReset;
+  deployments: typeof deployments;
+  fileSync: typeof fileSync;
+  github: typeof github;
+  githubAutoCreate: typeof githubAutoCreate;
+  githubHelpers: typeof githubHelpers;
+  githubQueries: typeof githubQueries;
+  githubStorage: typeof githubStorage;
+  githubSync: typeof githubSync;
+  githubSyncHelpers: typeof githubSyncHelpers;
+  githubWebhooks: typeof githubWebhooks;
+  hfRagSpace: typeof hfRagSpace;
   http: typeof http;
+  obscureRepoGenerator: typeof obscureRepoGenerator;
+  qemuSandbox: typeof qemuSandbox;
+  rag: typeof rag;
+  ragHelpers: typeof ragHelpers;
+  sandbox: typeof sandbox;
+  sandboxHelpers: typeof sandboxHelpers;
+  study: typeof study;
+  studyHelpers: typeof studyHelpers;
   users: typeof users;
+  v86Sandbox: typeof v86Sandbox;
 }>;
 
 /**
