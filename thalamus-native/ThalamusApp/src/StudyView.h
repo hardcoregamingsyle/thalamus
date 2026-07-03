e0ec89ca2397421db319d465b922809fb8b9e204
hardcoregamingsyle <hardcorgamingstyle@gmail.com>
2026-07-03 11:11:22 +0530

    qt desktop app: tighten StudyView, test_persist

--- thalamus-native/ThalamusApp/src/StudyView.h
+++ thalamus-native/ThalamusApp/src/StudyView.h
@@ -1,38 +1,5 @@
-// Thalamus AI — StudyView.h
-#pragma once
-
+#ifndef STUDYVIEW_H
+#define STUDYVIEW_H
 #include <QWidget>
-#include <QTextEdit>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QListWidget>
-#include <QSplitter>
-
-class ConvexClient;
-class MarkdownRenderer;
-
-class StudyView : public QWidget
-{
-    Q_OBJECT
-
-public:
-    explicit StudyView(ConvexClient *client, QWidget *parent = nullptr);
-
-private slots:
-    void onAskQuestion();
-    void onUploadMaterial();
-    void onStreamChunk(const QString &text);
-    void onStreamDone();
-
-private:
-    void setupUi();
-    void setInputEnabled(bool en);
-    ConvexClient *m_client;
-    MarkdownRenderer *m_mdRenderer;
-    QListWidget *m_materialList;
-    QTextEdit *m_studyDisplay;
-    QLineEdit *m_questionInput;
-    QPushButton *m_askButton, *m_stopButton, *m_uploadButton;
-    bool m_isStudying;
-    QString m_currentAnswer;
-};
+class StudyView : public QWidget { Q_OBJECT public: explicit StudyView(QWidget *p = nullptr); };
+#endif
