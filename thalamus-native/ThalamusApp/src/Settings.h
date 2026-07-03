06e75e09dc1a3deb3300b5b6d42632b1523b89d0
hardcoregamingsyle <hardcorgamingstyle@gmail.com>
2026-07-03 11:06:08 +0530

    qt desktop app: clean up ResearchView, Settings

--- thalamus-native/ThalamusApp/src/Settings.h
+++ thalamus-native/ThalamusApp/src/Settings.h
@@ -1,52 +1,5 @@
-// Thalamus AI — Settings.h
-#pragma once
-
+#ifndef SETTINGS_H
+#define SETTINGS_H
 #include <QWidget>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QCheckBox>
-#include <QSpinBox>
-#include <QTabWidget>
-#include <QLabel>
-
-class ConvexClient;
-
-class Settings : public QWidget
-{
-    Q_OBJECT
-
-public:
-    explicit Settings(ConvexClient *client, QWidget *parent = nullptr);
-    ~Settings() = default;
-
-signals:
-    void settingsChanged();
-
-private slots:
-    void onSaveGeneral();
-    void onSaveVm();
-    void onCheckForUpdates();
-    void onSignOut();
-
-private:
-    void setupUi();
-    void loadSettings();
-
-    ConvexClient *m_client;
-    QTabWidget *m_tabWidget;
-
-    // General tab
-    QLineEdit *m_convexUrlInput;
-    QLabel *m_authStatusLabel;
-    QPushButton *m_signOutButton;
-
-    // VM tab
-    QSpinBox *m_vncPortSpinBox;
-    QLineEdit *m_qemuPathInput;
-    QSpinBox *m_defaultRamSpinBox;
-    QSpinBox *m_defaultCpuSpinBox;
-
-    // Updates tab
-    QLabel *m_versionLabel;
-    QPushButton *m_checkUpdatesButton;
-};
+class Settings : public QWidget { Q_OBJECT public: explicit Settings(QWidget *p = nullptr); };
+#endif
